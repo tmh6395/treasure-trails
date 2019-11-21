99d0c63682a31f334bd369b40f58ed0f2274476c
Taylor <UkrainianEagle@q.com>
2019-11-21 00:52:27 -0600

    Made main menu look nicer

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,75 +7,55 @@ class Home extends Component {
 	render() {
 		return (<>
 			<div id="main-design">
-				<Row className="main-row-links">
-					<Col xs={2}></Col>
-					<Col>
+				<Row className="main-row-links header-row">
+					<Col></Col>
+					<Col xs={12} md={8} lg={4} id="main-col-links">
 						<Link
 							to={"/treasure-trails-helper/anagrams"}>
 							<p className="main-menu-options">Anagrams</p>
 						</Link>
-					</Col>
-					<Col>
+						
 						<Link
 							to={"/treasure-trails-helper/challenges"}>
 							<p className="main-menu-options">Challenges</p>
 						</Link>
-					</Col>
-					<Col>
+						
 						<Link
 							to={"/treasure-trails-helper/coordinates"}>
 							<p className="main-menu-options">Coordinates</p>
 						</Link>
-					</Col>
-					<Col xs={2}></Col>
-				</Row>
-
-
-				<Row className="main-row-links">
-					<Col xs={2}></Col>
-					<Col>
+						
 						<Link
 							to={"/treasure-trails-helper/cryptics"}>
 							<p className="main-menu-options">Cryptics</p>
 						</Link>
-					</Col>
-					<Col>
+						
 						<Link
 							to={"/treasure-trails-helper/lockboxes"}>
 							<p className="main-menu-options">Lockboxes</p>
 						</Link>
-					</Col>
-					<Col>
+						
 						<Link
 							to={"/treasure-trails-helper/maps"}>
 							<p className="main-menu-options">Maps</p>
 						</Link>
-					</Col>
-					<Col xs={2}></Col>
-				</Row>
-
-
-				<Row className="main-row-links">
-					<Col xs={2}></Col>
-					<Col>
+						
 						<Link
 							to={"/treasure-trails-helper/puzzleboxguide"}>
 							<p className="main-menu-options">Puzzle Box Guide</p>
 						</Link>
-					</Col>
-					<Col>
+						
 						<Link
 							to={"/treasure-trails-helper/scans"}>
 							<p className="main-menu-options">Scans</p>
 						</Link>
-					</Col>
-					<Col>
+						
 						<Link
 							to={"/treasure-trails-helper/skillingriddles"}>
 							<p className="main-menu-options">Skilling Riddles</p>
 						</Link>
 					</Col>
-					<Col xs={2}></Col>
+					<Col></Col>
 				</Row>
 
 				{/* Simple solution for pages where #btn-to-top doesn't exist, so the scrollFunction doesn't cry "error!" */}
