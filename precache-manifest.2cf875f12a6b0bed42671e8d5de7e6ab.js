4e57d255c14eb806a071b5c0070082fd35a2a880
Taylor <UkrainianEagle@q.com>
2019-11-21 23:07:59 -0600

    Updates

--- precache-manifest.2cf875f12a6b0bed42671e8d5de7e6ab.js
+++ precache-manifest.2cf875f12a6b0bed42671e8d5de7e6ab.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd46d0cd9f47c02c00e763973988671e",
+    "revision": "d02dec6dc54a1887396fbd9968a17ae2",
     "url": "/treasure-trails-helper/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/treasure-trails-helper/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "ef62951907f2ad37f0bb",
-    "url": "/treasure-trails-helper/static/css/main.70c676f2.chunk.css"
+    "revision": "6f6056c9964215051bff",
+    "url": "/treasure-trails-helper/static/css/main.e0eb1270.chunk.css"
   },
   {
     "revision": "d406ec81a83cd183396b",
     "url": "/treasure-trails-helper/static/js/2.bf8f094a.chunk.js"
   },
   {
-    "revision": "ef62951907f2ad37f0bb",
-    "url": "/treasure-trails-helper/static/js/main.cbb51df1.chunk.js"
+    "revision": "6f6056c9964215051bff",
+    "url": "/treasure-trails-helper/static/js/main.1830324f.chunk.js"
   },
   {
     "revision": "495411c1253b71a0f928",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treasure-trails-helper/precache-manifest.91c88bb1dc3ade6b161a0d1f2497eaa2.js"
+  "/treasure-trails-helper/precache-manifest.2cf875f12a6b0bed42671e8d5de7e6ab.js"
 );
 
 self.addEventListener('message', (event) => {
